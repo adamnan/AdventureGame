14f4803be918a1c2cd5082fa6363f41c2f0bdc7e
adamnhy <adam@adamnhydeMacBook-Pro.local>
2015-01-06 21:17:46 -0500

    picture for the frame.

--- AdventureGame.py
+++ AdventureGame.py
@@ -1,1 +1,20 @@
+#! /usr/bin/env python
 import wx
+
+class MazeFrame(wx.Frame):
+
+    def __init__(self, parent):
+
+        wx.Frame.__init__(self, parent, wx.ID_ANY, "Our Title")
+
+        self.panel = wx.Panel(self)
+
+# ---------- Main Program Below ----------
+
+app = wx.App(False)
+
+initFrame = MazeFrame(None)
+
+initFrame.Show()
+
+app.MainLoop()
