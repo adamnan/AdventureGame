c08676cc5c6e58d872ca06e62592f0f9f7519214
Tommy Lin <tommylin1998@outlook.com>
2015-01-08 17:08:23 -0500

    I add more frames

--- AdventureGame.py
+++ AdventureGame.py
@@ -2,7 +2,9 @@
 import wx
 
 class Frame1(wx.Frame):
+
 	def __init__(self, parent):
+	
 		wx.Frame.__init__(self, parent, wx.ID_ANY, "Maze Runner", pos=(150, 200), size=(864,510))
 		self.initPictureFile = wx.Image("1.png", wx.BITMAP_TYPE_ANY)
 		self.initPictureBitmap = self.initPictureFile.ConvertToBitmap()
@@ -48,12 +50,71 @@ class Frame3(wx.Frame):
 	def __init__(self, parent):
 
 		wx.Frame.__init__(self, parent, wx.ID_ANY, "Maze Runner", pos=(150, 200), size=(864,510))
+		self.dilemmaLFile = wx.Image("2.png", wx.BITMAP_TYPE_ANY)
+		self.dilemmaLBitmap = self.dilemmaLFile.ConvertToBitmap()
+		self.dilemmaL = wx.StaticBitmap(self, wx.ID_ANY, self.dilemmaLBitmap, pos=(5,5))
+		self.panel = wx.Panel(self)
+		
+		btn3 = wx.Button(self, label="Go Left", pos=(273,200), size=(70,50))
+		btn3.Bind(wx.EVT_BUTTON, self.OnBtn3)
+		
+		btn4 = wx.Button(self, label="Go Right", pos=(427,200), size=(70,50))
+		btn4.Bind(wx.EVT_BUTTON, self.OnBtn4)
+		
+	def OnBtn3(self,e):
+		self.Hide()
+		secondWindow = Frame1(self)
+		secondWindow.Show()
+		
+	def OnBtn4(self, e):
+		self.Hide()
+		secondWindow = Frame6(self)
+		secondWindow.Show()
 		
 class Frame4(wx.Frame):
 
 	def __init__(self, parent):
 
 		wx.Frame.__init__(self, parent, wx.ID_ANY, "Maze Runner", pos=(150, 200), size=(864,510))
+		self.dilemmaRFile = wx.Image("2.png", wx.BITMAP_TYPE_ANY)
+		self.dilemmaRBitmap = self.dilemmaRFile.ConvertToBitmap()
+		self.dilemmaR = wx.StaticBitmap(self, wx.ID_ANY, self.dilemmaRBitmap, pos=(5,5))
+		self.panel = wx.Panel(self)
+		
+		btn5 = wx.Button(self, label="Go Left", pos=(273,200), size=(70,50))
+		btn5.Bind(wx.EVT_BUTTON, self.OnBtn5)
+		
+		btn6 = wx.Button(self, label="Go Right", pos=(427,200), size=(70,50))
+		btn6.Bind(wx.EVT_BUTTON, self.OnBtn6)
+		
+	def OnBtn5(self,e):
+		self.Hide()
+		secondWindow = Frame5(self)
+		secondWindow.Show()
+		
+	def OnBtn6(self, e):
+		self.Hide()
+		secondWindow = Frame1(self)
+		secondWindow.Show()
+		
+class Frame5(wx.Frame):
+
+	def __init__(self, parent):
+
+		wx.Frame.__init__(self, parent, wx.ID_ANY, "Maze Runner", pos=(150, 200), size=(864,510))
+		self.keyFile = wx.Image("3.png", wx.BITMAP_TYPE_ANY)
+		self.keyBitmap = self.keyFile.ConvertToBitmap()
+		self.key = wx.StaticBitmap(self, wx.ID_ANY, self.keyBitmap, pos=(5,5))
+		self.panel = wx.Panel(self)
+		
+		btn = wx.Button(self, label="Pick it up", pos=(357,350), size=(150,50))
+		btn.Bind(wx.EVT_BUTTON, self.OnBtn)
+		
+	def OnBtn(self, e):
+		self.Hide()
+		secondWindow = Frame2(self)
+		secondWindow.Show()
+		
 
 
 # ---------- Main Program Below ----------
