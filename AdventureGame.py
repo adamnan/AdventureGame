1daad7a577579d2692dcd7b718350f774091d767
adamnhy <adam@adamnhydeMacBook-Pro.local>
2015-01-09 08:41:43 -0500

    added a death frame

--- AdventureGame.py
+++ AdventureGame.py
@@ -1,6 +1,8 @@
 #! /usr/bin/env python
 import wx
 
+DT = 0
+
 class Frame1(wx.Frame):
 
 	def __init__(self, parent):
@@ -50,7 +52,7 @@ class Frame3(wx.Frame):
 	def __init__(self, parent):
 
 		wx.Frame.__init__(self, parent, wx.ID_ANY, "Maze Runner", pos=(150, 200), size=(864,510))
-		self.dilemmaLFile = wx.Image("2.png", wx.BITMAP_TYPE_ANY)
+		self.dilemmaLFile = wx.Image("3.png", wx.BITMAP_TYPE_ANY)
 		self.dilemmaLBitmap = self.dilemmaLFile.ConvertToBitmap()
 		self.dilemmaL = wx.StaticBitmap(self, wx.ID_ANY, self.dilemmaLBitmap, pos=(5,5))
 		self.panel = wx.Panel(self)
@@ -63,7 +65,7 @@ class Frame3(wx.Frame):
 		
 	def OnBtn3(self,e):
 		self.Hide()
-		secondWindow = Frame1(self)
+		secondWindow = FrameDeath(self)
 		secondWindow.Show()
 		
 	def OnBtn4(self, e):
@@ -76,7 +78,7 @@ class Frame4(wx.Frame):
 	def __init__(self, parent):
 
 		wx.Frame.__init__(self, parent, wx.ID_ANY, "Maze Runner", pos=(150, 200), size=(864,510))
-		self.dilemmaRFile = wx.Image("2.png", wx.BITMAP_TYPE_ANY)
+		self.dilemmaRFile = wx.Image("4.png", wx.BITMAP_TYPE_ANY)
 		self.dilemmaRBitmap = self.dilemmaRFile.ConvertToBitmap()
 		self.dilemmaR = wx.StaticBitmap(self, wx.ID_ANY, self.dilemmaRBitmap, pos=(5,5))
 		self.panel = wx.Panel(self)
@@ -84,17 +86,22 @@ class Frame4(wx.Frame):
 		btn5 = wx.Button(self, label="Go Left", pos=(273,200), size=(70,50))
 		btn5.Bind(wx.EVT_BUTTON, self.OnBtn5)
 		
-		btn6 = wx.Button(self, label="Go Right", pos=(427,200), size=(70,50))
+		btn6 = wx.Button(self, label="Go Right", pos=(457,200), size=(70,50))
 		btn6.Bind(wx.EVT_BUTTON, self.OnBtn6)
 		
 	def OnBtn5(self,e):
-		self.Hide()
-		secondWindow = Frame5(self)
-		secondWindow.Show()
+		if DT > 1:		
+			self.Hide()
+			secondWindow = Frame5(self)
+			secondWindow.Show()
+		else:
+			self.Hide()
+			secondWindow = FrameDeath(self)
+			secondWindow.Show()
 		
 	def OnBtn6(self, e):
 		self.Hide()
-		secondWindow = Frame1(self)
+		secondWindow = FrameDeath(self)
 		secondWindow.Show()
 		
 class Frame5(wx.Frame):
@@ -102,7 +109,7 @@ class Frame5(wx.Frame):
 	def __init__(self, parent):
 
 		wx.Frame.__init__(self, parent, wx.ID_ANY, "Maze Runner", pos=(150, 200), size=(864,510))
-		self.keyFile = wx.Image("3.png", wx.BITMAP_TYPE_ANY)
+		self.keyFile = wx.Image("5.png", wx.BITMAP_TYPE_ANY)
 		self.keyBitmap = self.keyFile.ConvertToBitmap()
 		self.key = wx.StaticBitmap(self, wx.ID_ANY, self.keyBitmap, pos=(5,5))
 		self.panel = wx.Panel(self)
@@ -112,9 +119,60 @@ class Frame5(wx.Frame):
 		
 	def OnBtn(self, e):
 		self.Hide()
+		secondWindow = Frame7(self)
+		secondWindow.Show()
+		
+class Frame6(wx.Frame):
+
+	def __init__(self, parent):
+
+		wx.Frame.__init__(self, parent, wx.ID_ANY, "Maze Runner", pos=(150, 200), size=(864,510))
+		self.keyFile = wx.Image("6.png", wx.BITMAP_TYPE_ANY)
+		self.keyBitmap = self.keyFile.ConvertToBitmap()
+		self.key = wx.StaticBitmap(self, wx.ID_ANY, self.keyBitmap, pos=(5,5))
+		self.panel = wx.Panel(self)
+		
+		btn = wx.Button(self, label="Back", pos=(357,350), size=(150,50))
+		btn.Bind(wx.EVT_BUTTON, self.OnBtn)
+		
+	def OnBtn(self, e):
+		self.Hide()
 		secondWindow = Frame2(self)
 		secondWindow.Show()
 		
+class Frame7(wx.Frame):
+
+	def __init__(self, parent):
+
+		wx.Frame.__init__(self, parent, wx.ID_ANY, "Maze Runner", pos=(150, 200), size=(864,510))
+		self.keyFile = wx.Image("1.png", wx.BITMAP_TYPE_ANY)
+		self.keyBitmap = self.keyFile.ConvertToBitmap()
+		self.key = wx.StaticBitmap(self, wx.ID_ANY, self.keyBitmap, pos=(5,5))
+		self.panel = wx.Panel(self)
+		
+		btn = wx.Button(self, label="Go on", pos=(357,350), size=(150,50))
+		btn.Bind(wx.EVT_BUTTON, self.OnBtn)
+		
+	def OnBtn(self, e):
+		self.Hide()
+		secondWindow = Frame8(self)
+		secondWindow.Show()
+		
+class FrameDeath(wx.Frame):
+
+	def __init__(self, parent):
+
+		wx.Frame.__init__(self, parent, wx.ID_ANY, "Maze Runner", pos=(150, 200), size=(864,510))
+		self.panel = wx.Panel(self)
+		
+		btn = wx.Button(self, label="Respawn", pos=(357,350), size=(150,50))
+		btn.Bind(wx.EVT_BUTTON, self.OnBtn)
+		
+	def OnBtn(self, e):
+		DT += 1
+		self.Hide()
+		secondWindow = Frame2(self)
+		secondWindow.Show()
 
 
 # ---------- Main Program Below ----------
